c1990729275f8385c252523b0d49cf8a5750e990
Karthik <karthik.pv77@gmail.com>
2025-09-28 19:43:46 +0530

    setup for mac

---
+++ start.sh
@@ -0,0 +1,11 @@
+# Navigate to the venv activation directory
+cd venv/bin
+
+# Activate the virtual environment (use 'source' to load it into the current shell)
+source activate
+
+# Navigate back to the AlgoTrading root directory
+cd ../..
+
+# Run the Python server (Use 'python3' instead of 'python')
+python3 server.py
